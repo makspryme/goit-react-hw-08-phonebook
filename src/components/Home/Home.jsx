2033e5a1d04bc6b4f6a1c466e9d5cc964f628043
makspryme <makspryme17@gmail.com>
2023-10-25 16:03:16 +0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,3 @@
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -6,8 +5,6 @@ export default function Home() {
   const Greeting = styled.h1({
     textAlign: 'center',
   });
-
-  const isLogged = useSelector(state => state.auth.isLoggedIn);
 
   return (
     <Greeting>
